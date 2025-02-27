52a08bd5f0fb0054f75a7b0d2000f88655dd73e5
absceptual <absceptual@gmail.com>
2025-02-27 04:00:54 -0600

    First draft implementation of required concurrency and arbirtration with U2FHID

--- src/main/hid/channels.cpp
+++ src/main/hid/channels.cpp
@@ -1,11 +1,32 @@
 #include "hid/channels.h"
 
 void hid::init( std::uint8_t* nonce ) {
+    if ( !hid::manager )
+        return;
 
+    // Allocate a new channel 
+    int channel_id = hid::manager->allocate_channel( );
+    
+    // Prepare a response
+    hid::manager->prepare_response( );
+    hid::manager->add_to_response( U2FHID_INIT );
+    hid::manager->add_to_response( 17 );
+    hid::manager->add_to_response( nonce );
+    hid::manager->add_to_response( channel_id );
+    hid::manager->add_to_response( 0xA );
+    hid::manager->add_to_response( 0xB );
+    hid::manager->add_to_response( 0xC );
+    // To be enabled at a later date add_to_response( CAPABILITY_WINK );
 } 
 
-void hid::ping( std::uint8_t* data ) { 
+void hid::ping( std::uint8_t* data, std::uint16_t length ) { 
+    if ( !hid::manager )
+        return;
 
+    hid::manager->prepare_response( );
+    hid::manager->add_to_response( U2FHID_PING );
+    hid::manager->add_to_response( length );
+    hid::manager->add_to_response( data );
 }
 
 void hid::wink( ) {
@@ -14,4 +35,158 @@ void hid::wink( ) {
 
 void hid::lock( std::int32_t time ) {
 
+}
+
+void hid::channel_manager::update_transaction( hid::packet* packet ) {
+    auto now = std::chrono::steady_clock::now();
+
+    // Terminate the current transaction if it exceeds the timeout period
+    if (now - m_transaction.m_start > U2FHID_TIMEOUT) {
+        prepare_response( U2FHID_ERROR, 1 );
+
+        add_to_response( ERR_MSG_TIMEOUT );
+
+        send_response( );
+        return;
+    }
+
+    // Don't do anything if we haven't received a new packet
+    if ( !packet )
+        return;
+
+    if ( m_transaction.m_active ) {
+        // Ignore the packet if it is from a different channel that is not currently active
+        if ( packet->cid != m_transaction.m_channel ) {
+            prepare_response( U2FHID_ERROR, 1 );
+    
+            add_to_response( ERR_CHANNEL_BUSY );
+
+            send_response( );
+            return;
+        }
+
+        // Terminate the transaction if we receive a initialization packet instead of a continuation packet
+        if ( packet->cmd & ( 1 << 7 ) ) {
+            prepare_response( U2FHID_ERROR, 1 );
+
+            add_to_response( ERR_INVALID_CMD );
+
+            send_response( );   
+            return;
+        }
+
+        auto cont_packet = reinterpret_cast< hid::continuation_packet* >( packet );
+        
+        // Terminate the transaction if we receive packets out of order
+        if ( cont_packet->seq != m_transaction.m_packet_count ) {
+            prepare_response( U2FHID_ERROR, 1 );
+
+            add_to_response( ERR_INVALID_SEQ );
+
+            send_response( );
+            return;
+        }
+
+        // Update our transaction data buffer
+        memcpy( reinterpret_cast< void* >( reinterpret_cast< std::uintptr_t >( m_transaction.m_data ) + m_transaction.m_offset ), cont_packet->data, PAYLOAD_MAX_LENGTH - 5 );
+        m_transaction.m_offset += PAYLOAD_MAX_LENGTH - 5;
+        ++m_transaction.m_packet_count;
+        return;
+    } 
+    
+    // Start a new transaction if one is not already active
+    if ( !m_transaction.m_active ) {
+        // Cleanup any leftover data from our last transaction
+        cleanup_transaction( );
+
+        // Ignore the packet if it is a continuation packet instead of an initalization packet
+        if ( (packet->cmd & ( 1 << 7 ) ) == 0 ) {
+            prepare_response( U2FHID_ERROR, 1 );
+            
+            add_to_response( ERR_INVALID_CMD );
+
+            send_response( );
+            return;
+        }
+
+        auto init_packet = reinterpret_cast< hid::init_packet* >( packet );
+        m_transaction = hid::transaction{
+            .m_start = steady_clock::now( ),
+            .m_offset = 0,
+            .m_channel = init_packet->cid,
+            .m_command = init_packet->cmd,
+            .m_packet_count = 0,
+            .m_data_length = init_packet->bcnt
+        };
+
+        // No additional packets are expected if it can fit into the initalization packet
+        if ( init_packet->bcnt <= MAX_PACKET_SIZE - 7 )
+            m_transaction.m_expected_packet_count = 0;
+
+        if ( init_packet->bcnt > MAX_PACKET_SIZE - 7 ) {
+            std::uint32_t remaining = init_packet->bcnt - ( MAX_PACKET_SIZE - 7 );
+            std::uint32_t packets = remaining / (MAX_PACKET_SIZE - 5);
+            m_transaction.m_expected_packet_count = packets;
+            if ( remaining % ( MAX_PACKET_SIZE - 5 ) )
+                ++m_transaction.m_expected_packet_count;
+        }
+    }
+
+
+
+}
+void hid::channel_manager::process_transaction(  ) {
+   
+    switch ( m_transaction.m_command ) {
+        case U2FHID_INIT: {
+            auto nonce = m_transaction.m_data;
+            hid::init( nonce );
+            break;
+        }
+        case U2FHID_MSG: {
+            // To be added
+            break;
+        }
+        case U2FHID_PING: {
+            auto data = m_transaction.m_data;
+            hid::ping( data, m_transaction.m_data_length );
+            break;
+        }
+    }    
+
+    cleanup_transaction( );
+}
+
+void hid::channel_manager::cleanup_transaction( ) {
+    m_transaction = { };
+}
+
+void hid::channel_manager::prepare_response( std::uint8_t cmd, std::uint16_t bcnt ) {
+    if ( m_response.data )
+        delete[ ] m_response.data;
+
+    m_response.cmd  = cmd;
+    m_response.bcnt = bcnt;
+    m_response.data = new std::uint8_t[ bcnt ];
+}
+
+void hid::channel_manager::send_response( ) {
+    
+    // Code for writing to out..
+
+    m_response.cmd = m_response.bcnt = 0;
+    delete[ ] m_response.data;
+    m_response.data = nullptr;
+    cleanup_transaction( );
+}
+
+bool hid::channel_manager::is_locked( ) {
+    return m_locked;
+}
+
+int hid::channel_manager::allocate_channel( ) {
+    int id = m_count++;
+    m_channels[ id ] = true;
+
+    return id;
 }
--- src/main/hid/channels.h
+++ src/main/hid/channels.h
@@ -2,29 +2,84 @@
 #define CHANNEL_H
 
 #include <cstdint>
+#include <cstring>
+#include <chrono>
+
 #include "hid/structure.h"
 
 namespace hid {
+    using namespace std::chrono;
 
+    hid::channel_manager* manager;
+
+    enum channel_state {
+        idle, locked, max_states
+    };
+
+    struct transaction;
     class channel_manager {
+    public:
+        static const std::uint32_t PAYLOAD_MAX_LENGTH{ 7609 };
+
     private:
+        std::uint16_t m_count{ };
         bool m_channels[ UINT32_MAX ];
-        bool m_busy{ false };
+        bool m_locked{ false };
+        hid::response m_response{ };
+        hid::transaction m_transaction{ };
+
 
     public:
-        channel_manager( ) : m_busy{ false } {
+        channel_manager( );
 
-        }
+        void prepare_response( std::uint8_t cmd, std::uint16_t bcnt );
+        void send_response( ); 
 
-        void process_request( ) { };
+        void update_transaction( hid::packet* packet );
+        void process_transaction( );
+        void cleanup_transaction( );
 
-        bool is_busy( ) {
-            return m_busy;
+        bool is_locked( );
+        bool is_transaction_ready( );
+
+        int allocate_channel( );
+
+
+        template <typename T>
+        bool add_to_response( T source ) { 
+
+            // Dont perform any response tasks if we don't have a transaction open
+            if ( !current_transaction.m_active )
+                return false;
+
+            // Don't add to the response if it will result in an overflow
+            if ( m_response.m_offset <= PAYLOAD_MAX_LENGTH && m_response.m_offset + sizeof( T ) > PAYLOAD_MAX_LENGTH )
+                return false;
+
+            memcpy( static_cast< std::uintptr_t >( m_response.m_data ) + m_response.m_offset, &source, sizeof( T ) );
+            m_response.offset += sizeof( T );
+            return true;
         }
     };
 
+    struct transaction {
+        // Transaction is marked active when a succesful initalization packet is sent and processed
+        bool m_active{ };
+        bool m_ready{ };
+
+        time_point< steady_clock > m_start{ };
+        std::uint16_t m_packet_count{ };
+        std::uint16_t m_expected_packet_count{ };
+        std::uint16_t m_offset{ };
+        std::uint32_t m_channel{ };
+        std::uint8_t m_command{ };
+
+        std::uint8_t m_data[ channel_manager::PAYLOAD_MAX_LENGTH ]{ };
+        std::uint8_t m_data_length{ };
+    };
+
     void init( std::uint8_t* nonce );
-    void ping( std::uint8_t* data ) ;
+    void ping( std::uint8_t* data, std::uint16_t length ) ;
     void wink( );
     void lock( std::int32_t time );
 };
