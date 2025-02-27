2a92c626452ad082cbf909c50980a1c9e05022fd
absceptual <absceptual@gmail.com>
2025-02-27 03:59:11 -0600

    Psuedocode logic for packets

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,9 +1,17 @@
-#include "headers/messages.h"
+#include "headers/requests.h"
 #include "hid/channels.h"
 
+bool get_available_packets( );
 
-void loop( ) {
+int loop( ) {
 
-    // 
+    hid::channel_manager* manager = new hid::channel_manager{ };
+
+    while ( true ) {       
+        auto packet = get_available_packets( );
+        manager->update_transaction( reinterpret_cast< hid::packet* >( &packet ) );
+        if ( manager->is_transaction_ready( ) ) 
+            manager->process_transaction( );
+    }
     hid::init_packet data;
 }
